8c583b6a3f4ff299c5e218230023929c4fdc004d
Rafhael <rafhael.dev@gmail.com>
2024-04-20 21:38:47 -0300

    test: category validator

--- domain/src/test/java/com/studies/catalog/admin/domain/category/CategoryTest.java
+++ domain/src/test/java/com/studies/catalog/admin/domain/category/CategoryTest.java
@@ -1,5 +1,7 @@
 package com.studies.catalog.admin.domain.category;
 
+import com.studies.catalog.admin.domain.exceptions.DomainException;
+import com.studies.catalog.admin.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +23,23 @@ class CategoryTest {
         Assertions.assertNotNull(actualCategory.getCreatedAt());
         Assertions.assertNotNull(actualCategory.getUpdatedAt());
         Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReceiveOneError() {
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedDescription = "The most watched category";
+        final var expectedIsActive = true;
+
+        final var actualCategory =
+                Category.newCategory(null, expectedDescription, expectedIsActive);
+
+        final var actualException =
+                Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
     }
 
 }
