60f77dd9b50479a468a3fae750e239b34d14470b
Rafhael <rafhael.dev@gmail.com>
2024-04-22 15:45:47 -0300

    feat: basic structure for use cases

---
+++ application/src/main/java/com/studies/catalog/admin/application/NullaryUseCase.java
@@ -0,0 +1,7 @@
+package com.studies.catalog.admin.application;
+
+public abstract class NullaryUseCase<OUT> {
+
+    public abstract OUT execute();
+
+}
---
+++ application/src/main/java/com/studies/catalog/admin/application/UseCase.java
@@ -0,0 +1,7 @@
+package com.studies.catalog.admin.application;
+
+public abstract class UseCase<IN, OUT> {
+
+    public abstract OUT execute(IN anIn);
+
+}
