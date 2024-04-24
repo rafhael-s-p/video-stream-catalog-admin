4f3de9aef65dcc8ea6a2396b36349ea60d0c218d
Rafhael <rafhael.dev@gmail.com>
2024-04-24 15:16:25 -0300

    feat: category delete use case

---
+++ application/src/main/java/com/studies/catalog/admin/application/delete/DefaultDeleteCategoryUseCase.java
@@ -0,0 +1,21 @@
+package com.studies.catalog.admin.application.delete;
+
+import com.studies.catalog.admin.domain.category.CategoryGateway;
+import com.studies.catalog.admin.domain.category.CategoryID;
+
+import java.util.Objects;
+
+public class DefaultDeleteCategoryUseCase extends DeleteCategoryUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultDeleteCategoryUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public void execute(final String anIn) {
+        this.categoryGateway.deleteById(CategoryID.from(anIn));
+    }
+
+}
---
+++ application/src/main/java/com/studies/catalog/admin/application/delete/DeleteCategoryUseCase.java
@@ -0,0 +1,6 @@
+package com.studies.catalog.admin.application.delete;
+
+import com.studies.catalog.admin.application.UnitUseCase;
+
+public abstract class DeleteCategoryUseCase
+        extends UnitUseCase<String> {}
