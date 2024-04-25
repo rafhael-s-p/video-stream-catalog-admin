059e03d7054fb74340f67dc97cd5607af800a4f2
Rafhael <rafhael.dev@gmail.com>
2024-04-25 16:12:13 -0300

    feat: category jpa repository

---
+++ infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/category/persistence/CategoryRepository.java
@@ -0,0 +1,5 @@
+package com.studies.catalog.admin.infrastructure.category.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryJpaEntity, String> {}
