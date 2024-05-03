960f16a6a33230f2dab85d509ae317586cad20b7
Rafhael <rafhael.dev@gmail.com>
2024-05-03 08:14:25 -0300

    feat: category delete by id endpoint

--- infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/api/CategoryAPI.java
+++ infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/api/CategoryAPI.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -49,8 +50,22 @@ public interface CategoryAPI {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Category updated successfully"),
             @ApiResponse(responseCode = "404", description = "Category was not found"),
-            @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+            @ApiResponse(responseCode = "500", description = "An internal server error has occurred"),
     })
     ResponseEntity<?> updateById(@PathVariable(name = "id") String id, @RequestBody UpdateCategoryApiInput input);
+
+    @DeleteMapping(
+            value = "{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete a category by its identifier")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Category deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Category was not found"),
+            @ApiResponse(responseCode = "500", description = "An internal server error has occurred"),
+    })
+    void deleteById(@PathVariable(name = "id") String id);
 
 }
--- infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/api/controllers/CategoryController.java
@@ -3,6 +3,7 @@ package com.studies.catalog.admin.infrastructure.api.controllers;
 import com.studies.catalog.admin.application.create.CreateCategoryCommand;
 import com.studies.catalog.admin.application.create.CreateCategoryOutput;
 import com.studies.catalog.admin.application.create.CreateCategoryUseCase;
+import com.studies.catalog.admin.application.delete.DeleteCategoryUseCase;
 import com.studies.catalog.admin.application.retrieve.get.GetCategoryByIdUseCase;
 import com.studies.catalog.admin.application.update.UpdateCategoryCommand;
 import com.studies.catalog.admin.application.update.UpdateCategoryOutput;
@@ -26,15 +27,18 @@ public class CategoryController implements CategoryAPI {
     private final CreateCategoryUseCase createCategoryUseCase;
     private final GetCategoryByIdUseCase getCategoryByIdUseCase;
     private final UpdateCategoryUseCase updateCategoryUseCase;
+    private final DeleteCategoryUseCase deleteCategoryUseCase;
 
     public CategoryController(
             final CreateCategoryUseCase createCategoryUseCase,
             final GetCategoryByIdUseCase getCategoryByIdUseCase,
-            final UpdateCategoryUseCase updateCategoryUseCase
+            final UpdateCategoryUseCase updateCategoryUseCase,
+            final DeleteCategoryUseCase deleteCategoryUseCase
     ) {
         this.createCategoryUseCase = Objects.requireNonNull(createCategoryUseCase);
         this.getCategoryByIdUseCase = Objects.requireNonNull(getCategoryByIdUseCase);
         this.updateCategoryUseCase = Objects.requireNonNull(updateCategoryUseCase);
+        this.deleteCategoryUseCase = Objects.requireNonNull(deleteCategoryUseCase);
     }
 
     @Override
@@ -78,6 +82,11 @@ public class CategoryController implements CategoryAPI {
 
         return this.updateCategoryUseCase.execute(aCommand)
                 .fold(onError, onSuccess);
+    }
+
+    @Override
+    public void deleteById(final String anId) {
+        this.deleteCategoryUseCase.execute(anId);
     }
 
 }
