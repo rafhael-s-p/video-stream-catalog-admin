9cfbfbbf719c9a533296eb84674e21a7397d8c78
Rafhael <rafhael.dev@gmail.com>
2024-05-07 18:22:10 -0300

    test: genre update

--- domain/src/test/java/com/studies/catalog/admin/domain/genre/GenreTest.java
+++ domain/src/test/java/com/studies/catalog/admin/domain/genre/GenreTest.java
@@ -1,9 +1,13 @@
 package com.studies.catalog.admin.domain.genre;
 
+import com.studies.catalog.admin.domain.category.CategoryID;
 import com.studies.catalog.admin.domain.exceptions.NotificationException;
 import helpers.TestHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class GenreTest extends TestHelper {
 
@@ -116,6 +120,116 @@ class GenreTest extends TestHelper {
         Assertions.assertEquals(currentCreatedAt, currentGenre.getCreatedAt());
         Assertions.assertTrue(currentUpdatedAt.isBefore(currentGenre.getUpdatedAt()));
         Assertions.assertNotNull(currentGenre.getUpdatedAt());
+        Assertions.assertNull(currentGenre.getDeletedAt());
+    }
+
+    @Test
+    void givenAValidInactiveGenre_whenCallUpdateWithActivate_shouldReceiveGenreUpdated() {
+        final var expectedName = "Terror";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.of(CategoryID.from("789"));
+
+        final var currentGenre = Genre.newGenre("Nerror", false);
+
+        Assertions.assertNotNull(currentGenre);
+        Assertions.assertFalse(currentGenre.isActive());
+        Assertions.assertNotNull(currentGenre.getDeletedAt());
+
+        final var currentCreatedAt = currentGenre.getCreatedAt();
+        final var currentUpdatedAt = currentGenre.getUpdatedAt();
+
+        currentGenre.update(expectedName, expectedIsActive, expectedCategories);
+
+        Assertions.assertNotNull(currentGenre.getId());
+        Assertions.assertEquals(expectedName, currentGenre.getName());
+        Assertions.assertEquals(expectedIsActive, currentGenre.isActive());
+        Assertions.assertEquals(expectedCategories, currentGenre.getCategories());
+        Assertions.assertEquals(currentCreatedAt, currentGenre.getCreatedAt());
+        Assertions.assertTrue(currentUpdatedAt.isBefore(currentGenre.getUpdatedAt()));
+        Assertions.assertNull(currentGenre.getDeletedAt());
+    }
+
+    @Test
+    void givenAValidActiveGenre_whenCallUpdateWithInactivate_shouldReceiveGenreUpdated() {
+        final var expectedName = "Terror";
+        final var expectedIsActive = false;
+        final var expectedCategories = List.of(CategoryID.from("789"));
+
+        final var currentGenre = Genre.newGenre("Nerror", true);
+
+        Assertions.assertNotNull(currentGenre);
+        Assertions.assertTrue(currentGenre.isActive());
+        Assertions.assertNull(currentGenre.getDeletedAt());
+
+        final var currentCreatedAt = currentGenre.getCreatedAt();
+        final var currentUpdatedAt = currentGenre.getUpdatedAt();
+
+        currentGenre.update(expectedName, expectedIsActive, expectedCategories);
+
+        Assertions.assertNotNull(currentGenre.getId());
+        Assertions.assertEquals(expectedName, currentGenre.getName());
+        Assertions.assertEquals(expectedIsActive, currentGenre.isActive());
+        Assertions.assertEquals(expectedCategories, currentGenre.getCategories());
+        Assertions.assertEquals(currentCreatedAt, currentGenre.getCreatedAt());
+        Assertions.assertTrue(currentUpdatedAt.isBefore(currentGenre.getUpdatedAt()));
+        Assertions.assertNotNull(currentGenre.getDeletedAt());
+    }
+
+    @Test
+    void givenAValidGenre_whenCallUpdateWithEmptyName_shouldReceiveNotificationException() {
+        final var expectedName = " ";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.of(CategoryID.from("789"));
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var currentGenre = Genre.newGenre("Nerror", false);
+
+        final var currentException = Assertions.assertThrows(NotificationException.class,
+                () -> currentGenre.update(expectedName, expectedIsActive, expectedCategories));
+
+        Assertions.assertEquals(expectedErrorCount, currentException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, currentException.getErrors().get(0).message());
+    }
+
+    @Test
+    void givenAValidGenre_whenCallUpdateWithNullName_shouldReceiveNotificationException() {
+        final String expectedName = null;
+        final var expectedIsActive = true;
+        final var expectedCategories = List.of(CategoryID.from("789"));
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var currentGenre = Genre.newGenre("Nerror", false);
+
+        final var currentException = Assertions.assertThrows(NotificationException.class,
+                () -> currentGenre.update(expectedName, expectedIsActive, expectedCategories));
+
+        Assertions.assertEquals(expectedErrorCount, currentException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, currentException.getErrors().get(0).message());
+    }
+
+    @Test
+    void givenAValidGenre_whenCallUpdateWithNullCategories_shouldReceiveOK() {
+        final var expectedName = "Terror";
+        final var expectedIsActive = true;
+        final var expectedCategories = new ArrayList<CategoryID>();
+
+        final var currentGenre = Genre.newGenre("Nerror", expectedIsActive);
+
+        final var currentCreatedAt = currentGenre.getCreatedAt();
+        final var currentUpdatedAt = currentGenre.getUpdatedAt();
+
+        Assertions.assertDoesNotThrow(() -> {
+            currentGenre.update(expectedName, expectedIsActive, null);
+        });
+
+        Assertions.assertNotNull(currentGenre.getId());
+        Assertions.assertEquals(expectedName, currentGenre.getName());
+        Assertions.assertEquals(expectedIsActive, currentGenre.isActive());
+        Assertions.assertEquals(expectedCategories, currentGenre.getCategories());
+        Assertions.assertEquals(currentCreatedAt, currentGenre.getCreatedAt());
+        Assertions.assertTrue(currentUpdatedAt.isBefore(currentGenre.getUpdatedAt()));
         Assertions.assertNull(currentGenre.getDeletedAt());
     }
 
