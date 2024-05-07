38663c99719c21bd0322cdb874f2f81331533a5b
Rafhael <rafhael.dev@gmail.com>
2024-05-07 09:08:01 -0300

    feat: util class for nanoseconds resolution

---
+++ domain/src/main/java/com/studies/catalog/admin/domain/utils/InstantUtils.java
@@ -0,0 +1,15 @@
+package com.studies.catalog.admin.domain.utils;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public final class InstantUtils {
+
+    private InstantUtils() {
+    }
+
+    public static Instant now() {
+        return Instant.now().truncatedTo(ChronoUnit.MICROS);
+    }
+
+}
