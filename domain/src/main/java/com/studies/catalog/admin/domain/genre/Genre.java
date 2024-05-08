64ffdbee888a70117b93386ffb332dbbd9e68149
Rafhael <rafhael.dev@gmail.com>
2024-05-08 10:04:57 -0300

    feat: add and remove category

--- domain/src/main/java/com/studies/catalog/admin/domain/genre/Genre.java
+++ domain/src/main/java/com/studies/catalog/admin/domain/genre/Genre.java
@@ -136,6 +136,26 @@ public class Genre extends AggregateRoot<GenreID> {
             throw new NotificationException("Failed to create an Aggregate Genre", notification);
     }
 
+    public Genre addCategory(final CategoryID aCategoryID) {
+        if (aCategoryID == null) {
+            return this;
+        }
+        this.categories.add(aCategoryID);
+        this.updatedAt = InstantUtils.now();
+
+        return this;
+    }
+
+    public Genre removeCategory(final CategoryID aCategoryID) {
+        if (aCategoryID == null)
+            return this;
+
+        this.categories.remove(aCategoryID);
+        this.updatedAt = InstantUtils.now();
+
+        return this;
+    }
+
     @Override
     public void validate(final ValidationHandler handler) {
         new GenreValidator(this, handler).validate();
