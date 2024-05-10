fd3e49f33223940d0abf48e53c668c7b70d47871
Rafhael <rafhael.dev@gmail.com>
2024-05-09 21:11:46 -0300

    feat: genre get by id use case

---
+++ application/src/main/java/com/studies/catalog/admin/application/genre/retrieve/get/GetGenreByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.studies.catalog.admin.application.genre.retrieve.get;
+
+import com.studies.catalog.admin.application.UseCase;
+
+public abstract class GetGenreByIdUseCase extends UseCase<String, GenreOutput> {
+}
