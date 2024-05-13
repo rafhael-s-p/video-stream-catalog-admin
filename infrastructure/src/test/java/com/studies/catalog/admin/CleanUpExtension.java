eebee8e3b57a31e9ebd6683526ade51275a3a2f0
Rafhael <rafhael.dev@gmail.com>
2024-05-13 10:34:39 -0300

    test: fixes

--- infrastructure/src/test/java/com/studies/catalog/admin/CleanUpExtension.java
+++ infrastructure/src/test/java/com/studies/catalog/admin/CleanUpExtension.java
@@ -1,22 +1,25 @@
 package com.studies.catalog.admin;
 
+import com.studies.catalog.admin.infrastructure.category.persistence.CategoryRepository;
+import com.studies.catalog.admin.infrastructure.genre.persistence.GenreRepository;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Collection;
+import java.util.List;
 
 public class CleanUpExtension implements BeforeEachCallback {
 
     @Override
     public void beforeEach(final ExtensionContext context) {
-        final var repositories = SpringExtension
-                .getApplicationContext(context)
-                .getBeansOfType(CrudRepository.class)
-                .values();
+        final var appContext = SpringExtension.getApplicationContext(context);
 
-        cleanUp(repositories);
+        cleanUp(List.of(
+                appContext.getBean(GenreRepository.class),
+                appContext.getBean(CategoryRepository.class)
+        ));
     }
 
     private void cleanUp(final Collection<CrudRepository> repositories) {
