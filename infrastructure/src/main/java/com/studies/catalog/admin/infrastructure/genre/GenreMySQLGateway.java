21e645ca5b4b7083a579893d0c2f8c83c78b3e63
Rafhael <rafhael.dev@gmail.com>
2024-05-13 13:39:19 -0300

    feat: genre gateway create

---
+++ infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/genre/GenreMySQLGateway.java
@@ -0,0 +1,54 @@
+package com.studies.catalog.admin.infrastructure.genre;
+
+import com.studies.catalog.admin.domain.genre.Genre;
+import com.studies.catalog.admin.domain.genre.GenreGateway;
+import com.studies.catalog.admin.domain.genre.GenreID;
+import com.studies.catalog.admin.domain.pagination.Pagination;
+import com.studies.catalog.admin.domain.pagination.SearchQuery;
+import com.studies.catalog.admin.infrastructure.genre.persistence.GenreJpaEntity;
+import com.studies.catalog.admin.infrastructure.genre.persistence.GenreRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Component
+public class GenreMySQLGateway implements GenreGateway {
+
+    private final GenreRepository genreRepository;
+
+    public GenreMySQLGateway(final GenreRepository genreRepository) {
+        this.genreRepository = Objects.requireNonNull(genreRepository);
+    }
+
+    @Override
+    public Pagination<Genre> findAll(SearchQuery aQuery) {
+        return null;
+    }
+
+    @Override
+    public Optional<Genre> findById(GenreID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Genre create(final Genre aGenre) {
+        return save(aGenre);
+    }
+
+    @Override
+    public Genre update(Genre aGenre) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(GenreID anId) {
+
+    }
+
+    private Genre save(final Genre aGenre) {
+        return this.genreRepository.save(GenreJpaEntity.from(aGenre))
+                .toAggregate();
+    }
+    
+}
