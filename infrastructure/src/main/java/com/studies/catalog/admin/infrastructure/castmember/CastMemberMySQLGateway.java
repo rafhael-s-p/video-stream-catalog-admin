714402ccc0cebeddd97cfa12ea162703ca22c809
Rafhael <rafhael.dev@gmail.com>
2024-05-24 18:24:55 -0300

    feat: cast member mysql gateway

---
+++ infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/castmember/CastMemberMySQLGateway.java
@@ -0,0 +1,48 @@
+package com.studies.catalog.admin.infrastructure.castmember;
+
+import com.studies.catalog.admin.domain.castmember.CastMember;
+import com.studies.catalog.admin.domain.castmember.CastMemberGateway;
+import com.studies.catalog.admin.domain.castmember.CastMemberID;
+import com.studies.catalog.admin.domain.pagination.Pagination;
+import com.studies.catalog.admin.domain.pagination.SearchQuery;
+import com.studies.catalog.admin.infrastructure.castmember.persistence.CastMemberRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Component
+public class CastMemberMySQLGateway implements CastMemberGateway {
+
+    private final CastMemberRepository castMemberRepository;
+
+    public CastMemberMySQLGateway(final CastMemberRepository castMemberRepository) {
+        this.castMemberRepository = Objects.requireNonNull(castMemberRepository);
+    }
+
+    @Override
+    public Pagination<CastMember> findAll(final SearchQuery aQuery) {
+        return null;
+    }
+
+    @Override
+    public Optional<CastMember> findById(final CastMemberID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public CastMember create(final CastMember aCastMember) {
+        return null;
+    }
+
+    @Override
+    public CastMember update(final CastMember aCastMember) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(final CastMemberID anId) {
+
+    }
+
+}
