140ef5dee93b37b9514dfd0419b716451c95c533
Rafhael <rafhael.dev@gmail.com>
2024-05-27 11:01:36 -0300

    test: cast member mysql gateway find all

--- infrastructure/src/test/java/com/studies/catalog/admin/infrastructure/castmember/CastMemberMySQLGatewayTest.java
+++ infrastructure/src/test/java/com/studies/catalog/admin/infrastructure/castmember/CastMemberMySQLGatewayTest.java
@@ -4,11 +4,16 @@ import com.studies.catalog.admin.MySQLGatewayTest;
 import com.studies.catalog.admin.domain.castmember.CastMember;
 import com.studies.catalog.admin.domain.castmember.CastMemberID;
 import com.studies.catalog.admin.domain.castmember.CastMemberType;
+import com.studies.catalog.admin.domain.pagination.SearchQuery;
 import com.studies.catalog.admin.infrastructure.castmember.persistence.CastMemberJpaEntity;
 import com.studies.catalog.admin.infrastructure.castmember.persistence.CastMemberRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 import static com.studies.catalog.admin.Fixture.CastMember.type;
 import static com.studies.catalog.admin.Fixture.name;
@@ -168,6 +173,149 @@ class CastMemberMySQLGatewayTest {
 
         // then
         Assertions.assertTrue(currentMember.isEmpty());
+    }
+
+    @Test
+    void givenEmptyCastMembers_whenCallsFindAll_shouldReturnEmpty() {
+        // given
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "";
+        final var expectedSort = "name";
+        final var expectedDirection = "asc";
+        final var expectedTotal = 0;
+
+        final var aQuery =
+                new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        // when
+        final var currentPage = castMemberGateway.findAll(aQuery);
+
+        // then
+        Assertions.assertEquals(expectedPage, currentPage.currentPage());
+        Assertions.assertEquals(expectedPerPage, currentPage.perPage());
+        Assertions.assertEquals(expectedTotal, currentPage.total());
+        Assertions.assertEquals(expectedTotal, currentPage.items().size());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "vin,0,10,1,1,Vin Diesel",
+            "taran,0,10,1,1,Quentin Tarantino",
+            "jas,0,10,1,1,Jason Momoa",
+            "har,0,10,1,1,Kit Harington",
+            "MAR,0,10,1,1,Martin Scorsese",
+    })
+    void givenAValidTerm_whenCallsFindAll_shouldReturnFiltered(
+            final String expectedTerms,
+            final int expectedPage,
+            final int expectedPerPage,
+            final int expectedItemsCount,
+            final long expectedTotal,
+            final String expectedName
+    ) {
+        // given
+        mockMembers();
+
+        final var expectedSort = "name";
+        final var expectedDirection = "asc";
+
+        final var aQuery =
+                new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        // when
+        final var currentPage = castMemberGateway.findAll(aQuery);
+
+        // then
+        Assertions.assertEquals(expectedPage, currentPage.currentPage());
+        Assertions.assertEquals(expectedPerPage, currentPage.perPage());
+        Assertions.assertEquals(expectedTotal, currentPage.total());
+        Assertions.assertEquals(expectedItemsCount, currentPage.items().size());
+        Assertions.assertEquals(expectedName, currentPage.items().get(0).getName());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "name,asc,0,10,5,5,Jason Momoa",
+            "name,desc,0,10,5,5,Vin Diesel",
+            "createdAt,asc,0,10,5,5,Kit Harington",
+            "createdAt,desc,0,10,5,5,Martin Scorsese",
+    })
+    void givenAValidSortAndDirection_whenCallsFindAll_shouldReturnSorted(
+            final String expectedSort,
+            final String expectedDirection,
+            final int expectedPage,
+            final int expectedPerPage,
+            final int expectedItemsCount,
+            final long expectedTotal,
+            final String expectedName
+    ) {
+        // given
+        mockMembers();
+
+        final var expectedTerms = "";
+
+        final var aQuery =
+                new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        // when
+        final var currentPage = castMemberGateway.findAll(aQuery);
+
+        // then
+        Assertions.assertEquals(expectedPage, currentPage.currentPage());
+        Assertions.assertEquals(expectedPerPage, currentPage.perPage());
+        Assertions.assertEquals(expectedTotal, currentPage.total());
+        Assertions.assertEquals(expectedItemsCount, currentPage.items().size());
+        Assertions.assertEquals(expectedName, currentPage.items().get(0).getName());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0,2,2,5,Jason Momoa;Kit Harington",
+            "1,2,2,5,Martin Scorsese;Quentin Tarantino",
+            "2,2,1,5,Vin Diesel",
+    })
+    void givenAValidPagination_whenCallsFindAll_shouldReturnPaginated(
+            final int expectedPage,
+            final int expectedPerPage,
+            final int expectedItemsCount,
+            final long expectedTotal,
+            final String expectedNames
+    ) {
+        // given
+        mockMembers();
+
+        final var expectedTerms = "";
+        final var expectedSort = "name";
+        final var expectedDirection = "asc";
+
+        final var aQuery =
+                new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        // when
+        final var currentPage = castMemberGateway.findAll(aQuery);
+
+        // then
+        Assertions.assertEquals(expectedPage, currentPage.currentPage());
+        Assertions.assertEquals(expectedPerPage, currentPage.perPage());
+        Assertions.assertEquals(expectedTotal, currentPage.total());
+        Assertions.assertEquals(expectedItemsCount, currentPage.items().size());
+
+        int index = 0;
+        for (final var expectedName : expectedNames.split(";")) {
+            Assertions.assertEquals(expectedName, currentPage.items().get(index).getName());
+            index++;
+        }
+    }
+
+    private void mockMembers() {
+        castMemberRepository.saveAllAndFlush(List.of(
+                CastMemberJpaEntity.from(CastMember.newMember("Kit Harington", CastMemberType.ACTOR)),
+                CastMemberJpaEntity.from(CastMember.newMember("Vin Diesel", CastMemberType.ACTOR)),
+                CastMemberJpaEntity.from(CastMember.newMember("Quentin Tarantino", CastMemberType.DIRECTOR)),
+                CastMemberJpaEntity.from(CastMember.newMember("Jason Momoa", CastMemberType.ACTOR)),
+                CastMemberJpaEntity.from(CastMember.newMember("Martin Scorsese", CastMemberType.DIRECTOR))
+        ));
     }
 
 }
