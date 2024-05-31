959acf3eec23ff4f90cbf9ba9c29be08b923006e
Rafhael <rafhael.dev@gmail.com>
2024-05-31 20:15:17 -0300

    test: deserialization of the create cast member api request

---
+++ infrastructure/src/test/java/com/studies/catalog/admin/infrastructure/castmember/models/CreateCastMemberApiRequestTest.java
@@ -0,0 +1,34 @@
+package com.studies.catalog.admin.infrastructure.castmember.models;
+
+import com.studies.catalog.admin.Fixture;
+import com.studies.catalog.admin.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+@JacksonTest
+class CreateCastMemberApiRequestTest {
+
+    @Autowired
+    private JacksonTester<CreateCastMemberApiRequest> json;
+
+    @Test
+    void testUnmarshall() throws Exception {
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type();
+
+        final var json = """
+                {
+                  "name": "%s",
+                  "type": "%s"
+                }
+                """.formatted(expectedName, expectedType);
+
+        final var currentJson = this.json.parse(json);
+
+        Assertions.assertThat(currentJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("type", expectedType);
+    }
+}
