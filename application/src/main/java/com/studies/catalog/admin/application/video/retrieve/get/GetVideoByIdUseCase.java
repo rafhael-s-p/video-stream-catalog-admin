c3efa6e81cfb69ab1ff894338845a89b4cd9a322
Rafhael <rafhael.dev@gmail.com>
2024-06-06 09:55:27 -0300

    feat: video get by id use case

---
+++ application/src/main/java/com/studies/catalog/admin/application/video/retrieve/get/GetVideoByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.studies.catalog.admin.application.video.retrieve.get;
+
+import com.studies.catalog.admin.application.UseCase;
+
+public abstract class GetVideoByIdUseCase extends UseCase<String, VideoOutput> {
+}
