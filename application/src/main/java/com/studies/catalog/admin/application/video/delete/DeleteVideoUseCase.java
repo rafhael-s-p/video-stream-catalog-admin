878299d2b0596c81422ef742b3a281cbe3d15d88
Rafhael <rafhael.dev@gmail.com>
2024-06-06 09:34:22 -0300

    feat: video delete by id use case

---
+++ application/src/main/java/com/studies/catalog/admin/application/video/delete/DeleteVideoUseCase.java
@@ -0,0 +1,6 @@
+package com.studies.catalog.admin.application.video.delete;
+
+import com.studies.catalog.admin.application.UnitUseCase;
+
+public abstract class DeleteVideoUseCase extends UnitUseCase<String> {
+}
