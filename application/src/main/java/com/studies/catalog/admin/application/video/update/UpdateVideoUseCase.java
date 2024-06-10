15100169771987f9322e92642d3f3a8de4d7ed99
Rafhael <rafhael.dev@gmail.com>
2024-06-10 09:15:56 -0300

    feat: video update by id use case

---
+++ application/src/main/java/com/studies/catalog/admin/application/video/update/UpdateVideoUseCase.java
@@ -0,0 +1,7 @@
+package com.studies.catalog.admin.application.video.update;
+
+import com.studies.catalog.admin.application.UseCase;
+
+public abstract class UpdateVideoUseCase
+        extends UseCase<UpdateVideoInput, UpdateVideoOutput> {
+}
