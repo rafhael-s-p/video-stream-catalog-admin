f4835464286142895a41896246584126d7035246
Rafhael <rafhael.dev@gmail.com>
2024-06-20 11:25:10 -0300

    refactor: null if empty check

--- infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/video/DefaultVideoGateway.java
+++ infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/video/DefaultVideoGateway.java
@@ -12,8 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
+
+import static com.studies.catalog.admin.domain.utils.CollectionUtils.mapTo;
+import static com.studies.catalog.admin.domain.utils.CollectionUtils.nullIfEmpty;
 
 public class DefaultVideoGateway implements VideoGateway {
 
@@ -33,9 +34,9 @@ public class DefaultVideoGateway implements VideoGateway {
 
         final var currentPage = this.videoRepository.findAll(
                 SqlUtils.like(aQuery.terms()),
-                toString(aQuery.castMembers()),
-                toString(aQuery.categories()),
-                toString(aQuery.genres()),
+                nullIfEmpty(mapTo(aQuery.castMembers(), Identifier::getValue)),
+                nullIfEmpty(mapTo(aQuery.categories(), Identifier::getValue)),
+                nullIfEmpty(mapTo(aQuery.genres(), Identifier::getValue)),
                 page
         );
 
@@ -76,15 +77,6 @@ public class DefaultVideoGateway implements VideoGateway {
     private Video save(final Video aVideo) {
         return this.videoRepository.save(VideoJpaEntity.from(aVideo))
                 .toAggregate();
-    }
-
-    private Set<String> toString(final Set<? extends Identifier> ids) {
-        if (ids == null || ids.isEmpty())
-            return null;
-
-        return ids.stream()
-                .map(Identifier::getValue)
-                .collect(Collectors.toSet());
     }
 
 }
