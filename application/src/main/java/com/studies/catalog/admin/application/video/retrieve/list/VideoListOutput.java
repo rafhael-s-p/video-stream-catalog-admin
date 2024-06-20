1c5107a725b7059ab7f1e0bde4ff496ff708c84e
Rafhael <rafhael.dev@gmail.com>
2024-06-20 11:25:30 -0300

    feat: factory methods

--- application/src/main/java/com/studies/catalog/admin/application/video/retrieve/list/VideoListOutput.java
+++ application/src/main/java/com/studies/catalog/admin/application/video/retrieve/list/VideoListOutput.java
@@ -1,6 +1,7 @@
 package com.studies.catalog.admin.application.video.retrieve.list;
 
 import com.studies.catalog.admin.domain.video.Video;
+import com.studies.catalog.admin.domain.video.VideoPreview;
 
 import java.time.Instant;
 
@@ -19,6 +20,16 @@ public record VideoListOutput(
                 aVideo.getDescription(),
                 aVideo.getCreatedAt(),
                 aVideo.getUpdatedAt()
+        );
+    }
+
+    public static VideoListOutput from(final VideoPreview aVideo) {
+        return new VideoListOutput(
+                aVideo.id(),
+                aVideo.title(),
+                aVideo.description(),
+                aVideo.createdAt(),
+                aVideo.updatedAt()
         );
     }
 
