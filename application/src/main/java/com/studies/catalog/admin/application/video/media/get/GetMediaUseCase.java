c9a310ce4646db68048def7d9b1f6469f2d7d214
Rafhael <rafhael.dev@gmail.com>
2024-07-05 21:54:19 -0300

    feat: video media get use case

---
+++ application/src/main/java/com/studies/catalog/admin/application/video/media/get/GetMediaUseCase.java
@@ -0,0 +1,7 @@
+package com.studies.catalog.admin.application.video.media.get;
+
+import com.studies.catalog.admin.application.UseCase;
+
+public abstract class GetMediaUseCase
+        extends UseCase<GetMediaInput, MediaOutput> {
+}
---
+++ application/src/main/java/com/studies/catalog/admin/application/video/media/get/MediaOutput.java
@@ -0,0 +1,17 @@
+package com.studies.catalog.admin.application.video.media.get;
+
+import com.studies.catalog.admin.domain.video.Resource;
+
+public record MediaOutput(
+        byte[] content,
+        String contentType,
+        String name
+) {
+    public static MediaOutput with(final Resource aResource) {
+        return new MediaOutput(
+                aResource.content(),
+                aResource.contentType(),
+                aResource.name()
+        );
+    }
+}
