81c48953b517b5f23f7234e4c0dc6c04519cf5db
Rafhael <rafhael.dev@gmail.com>
2024-07-09 11:55:10 -0300

    feat: upload media by type use case

---
+++ application/src/main/java/com/studies/catalog/admin/application/video/media/upload/UploadMediaUseCase.java
@@ -0,0 +1,7 @@
+package com.studies.catalog.admin.application.video.media.upload;
+
+import com.studies.catalog.admin.application.UseCase;
+
+public abstract class UploadMediaUseCase
+        extends UseCase<UploadMediaInput, UploadMediaOutput> {
+}
