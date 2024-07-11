866504dd8469a2e011cf5c118865e97314590ab4
Rafhael <rafhael.dev@gmail.com>
2024-07-11 14:25:55 -0300

    feat: delete video use case storage cleaning

--- application/src/main/java/com/studies/catalog/admin/application/video/delete/DeleteVideoUseCaseImpl.java
+++ application/src/main/java/com/studies/catalog/admin/application/video/delete/DeleteVideoUseCaseImpl.java
@@ -1,5 +1,6 @@
 package com.studies.catalog.admin.application.video.delete;
 
+import com.studies.catalog.admin.domain.video.MediaResourceGateway;
 import com.studies.catalog.admin.domain.video.VideoGateway;
 import com.studies.catalog.admin.domain.video.VideoID;
 
@@ -8,14 +9,19 @@ import java.util.Objects;
 public class DeleteVideoUseCaseImpl extends DeleteVideoUseCase {
 
     private final VideoGateway videoGateway;
+    private final MediaResourceGateway mediaResourceGateway;
 
-    public DeleteVideoUseCaseImpl(final VideoGateway videoGateway) {
+    public DeleteVideoUseCaseImpl(final VideoGateway videoGateway, final MediaResourceGateway mediaResourceGateway) {
         this.videoGateway = Objects.requireNonNull(videoGateway);
+        this.mediaResourceGateway = Objects.requireNonNull(mediaResourceGateway);
     }
 
     @Override
-    public void execute(final String anIn) {
-        this.videoGateway.deleteById(VideoID.from(anIn));
+    public void execute(final String anId) {
+        final var aVideoId = VideoID.from(anId);
+
+        this.videoGateway.deleteById(aVideoId);
+        this.mediaResourceGateway.clearResources(aVideoId);
     }
 
 }
