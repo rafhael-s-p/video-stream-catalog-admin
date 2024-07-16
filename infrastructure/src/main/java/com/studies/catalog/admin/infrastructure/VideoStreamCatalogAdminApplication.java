a9cd5c83a070f2a909f6ef3ed72a9a9479e2cc30
Rafhael <rafhael.dev@gmail.com>
2024-07-16 15:06:48 -0300

    feat: rabbitmq

--- infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/VideoStreamCatalogAdminApplication.java
+++ infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/VideoStreamCatalogAdminApplication.java
@@ -1,13 +1,20 @@
 package com.studies.catalog.admin.infrastructure;
 
 import com.studies.catalog.admin.infrastructure.configuration.WebServerConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VideoStreamCatalogAdminApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(WebServerConfig.class, args);
+    }
+
+    @RabbitListener(queues = "video.encoded.queue")
+    void dummy() {
+
     }
 
 }
