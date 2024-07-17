ecb12b7246c3c79b207872e0d12e40929f01c289
Rafhael <rafhael.dev@gmail.com>
2024-07-17 10:09:16 -0300

    refactor: method signature

--- domain/src/main/java/com/studies/catalog/admin/domain/events/DomainEventPublisher.java
+++ domain/src/main/java/com/studies/catalog/admin/domain/events/DomainEventPublisher.java
@@ -1,6 +1,6 @@
 package com.studies.catalog.admin.domain.events;
 
 @FunctionalInterface
-public interface DomainEventPublisher {
-    void publishEvent(DomainEvent event);
+public interface DomainEventPublisher<T extends DomainEvent> {
+    void publishEvent(T event);
 }
