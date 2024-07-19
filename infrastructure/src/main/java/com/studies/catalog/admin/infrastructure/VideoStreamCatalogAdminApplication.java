5903e4a3384f70bc8552dbcb30d7ad308c59d6a1
Rafhael <rafhael.dev@gmail.com>
2024-07-19 11:11:47 -0300

    refactor: adjustments

--- infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/VideoStreamCatalogAdminApplication.java
+++ infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/VideoStreamCatalogAdminApplication.java
@@ -1,7 +1,6 @@
 package com.studies.catalog.admin.infrastructure;
 
 import com.studies.catalog.admin.infrastructure.configuration.WebServerConfig;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,11 +9,6 @@ public class VideoStreamCatalogAdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebServerConfig.class, args);
-    }
-
-    @RabbitListener(queues = "video.encoded.queue")
-    void dummy() {
-
     }
 
 }
