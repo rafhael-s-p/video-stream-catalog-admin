7c142d290faa4b9c0a9cb2ed473e17f0c3055d88
Rafhael <rafhael.dev@gmail.com>
2024-07-22 09:43:16 -0300

    feat: video create endpoint

---
+++ infrastructure/src/main/java/com/studies/catalog/admin/infrastructure/api/controllers/VideoController.java
@@ -0,0 +1,83 @@
+package com.studies.catalog.admin.infrastructure.api.controllers;
+
+import com.studies.catalog.admin.application.video.create.CreateVideoInput;
+import com.studies.catalog.admin.application.video.create.CreateVideoUseCase;
+import com.studies.catalog.admin.domain.video.Resource;
+import com.studies.catalog.admin.infrastructure.api.VideoAPI;
+import com.studies.catalog.admin.infrastructure.utils.HashingUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URI;
+import java.util.Objects;
+import java.util.Set;
+
+@RestController
+public class VideoController implements VideoAPI {
+
+    private final CreateVideoUseCase createVideoUseCase;
+
+    public VideoController(final CreateVideoUseCase createVideoUseCase) {
+        this.createVideoUseCase = Objects.requireNonNull(createVideoUseCase);
+    }
+
+    @Override
+    public ResponseEntity<?> createFull(
+            final String aTitle,
+            final String aDescription,
+            final Integer launchedAt,
+            final Double aDuration,
+            final Boolean wasOpened,
+            final Boolean wasPublished,
+            final String aRating,
+            final Set<String> categories,
+            final Set<String> castMembers,
+            final Set<String> genres,
+            final MultipartFile videoFile,
+            final MultipartFile trailerFile,
+            final MultipartFile bannerFile,
+            final MultipartFile thumbFile,
+            final MultipartFile thumbHalfFile
+    ) {
+        final var anInput = CreateVideoInput.with(
+                aTitle,
+                aDescription,
+                launchedAt,
+                aDuration,
+                wasOpened,
+                wasPublished,
+                aRating,
+                categories,
+                genres,
+                castMembers,
+                resourceOf(videoFile),
+                resourceOf(trailerFile),
+                resourceOf(bannerFile),
+                resourceOf(thumbFile),
+                resourceOf(thumbHalfFile)
+        );
+
+        final var output = this.createVideoUseCase.execute(anInput);
+
+        return ResponseEntity.created(URI.create("/videos/" + output.id())).body(output);
+    }
+
+    private Resource resourceOf(final MultipartFile part) {
+        if (part == null) {
+            return null;
+        }
+
+        try {
+            return Resource.with(
+                    part.getBytes(),
+                    HashingUtils.checksum(part.getBytes()),
+                    part.getContentType(),
+                    part.getOriginalFilename()
+            );
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
+    }
+
+}
