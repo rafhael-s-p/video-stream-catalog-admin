0d84586fc80bdf379658f9cde4fa4d02952c9b07
Rafhael <rafhael.dev@gmail.com>
2024-07-22 17:57:24 -0300

    test: video create partial endpoint

--- infrastructure/src/test/java/com/studies/catalog/admin/infrastructure/api/VideoAPITest.java
+++ infrastructure/src/test/java/com/studies/catalog/admin/infrastructure/api/VideoAPITest.java
@@ -7,6 +7,7 @@ import com.studies.catalog.admin.application.video.create.CreateVideoOutput;
 import com.studies.catalog.admin.application.video.create.CreateVideoUseCase;
 import com.studies.catalog.admin.domain.Fixture;
 import com.studies.catalog.admin.domain.video.VideoID;
+import com.studies.catalog.admin.infrastructure.video.models.CreateVideoApiRequest;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -24,6 +25,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ControllerTest(controllers = VideoAPI.class)
@@ -123,6 +125,78 @@ class VideoAPITest {
         Assertions.assertEquals(expectedBanner.getOriginalFilename(), currentInput.getBanner().get().name());
         Assertions.assertEquals(expectedThumb.getOriginalFilename(), currentInput.getThumbnail().get().name());
         Assertions.assertEquals(expectedThumbHalf.getOriginalFilename(), currentInput.getThumbnailHalf().get().name());
+    }
+
+    @Test
+    void givenAValidInput_whenCallsCreatePartial_shouldReturnId() throws Exception {
+        // given
+        final var marlonBrando = Fixture.CastMembers.marlonBrando();
+        final var movies = Fixture.Categories.movies();
+        final var crime = Fixture.Genres.crime();
+
+        final var expectedId = VideoID.unique();
+        final var expectedTitle = Fixture.title();
+        final var expectedDescription = Fixture.Videos.description();
+        final var expectedLaunchYear = Year.of(Fixture.year());
+        final var expectedDuration = Fixture.duration();
+        final var expectedOpened = Fixture.bool();
+        final var expectedPublished = Fixture.bool();
+        final var expectedRating = Fixture.Videos.rating();
+        final var expectedCategories = Set.of(movies.getId().getValue());
+        final var expectedGenres = Set.of(crime.getId().getValue());
+        final var expectedMembers = Set.of(marlonBrando.getId().getValue());
+
+        final var anInput = new CreateVideoApiRequest(
+                expectedTitle,
+                expectedDescription,
+                expectedDuration,
+                expectedLaunchYear.getValue(),
+                expectedOpened,
+                expectedPublished,
+                expectedRating.getName(),
+                expectedMembers,
+                expectedCategories,
+                expectedGenres
+        );
+
+        when(createVideoUseCase.execute(any()))
+                .thenReturn(new CreateVideoOutput(expectedId.getValue()));
+
+        // when
+
+        final var aRequest = post("/videos")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(anInput));
+
+        this.mvc.perform(aRequest)
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/videos/" + expectedId.getValue()))
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id", Matchers.equalTo(expectedId.getValue())));
+
+        // then
+        final var inputCaptor = ArgumentCaptor.forClass(CreateVideoInput.class);
+
+        verify(createVideoUseCase).execute(inputCaptor.capture());
+
+        final var currentInput = inputCaptor.getValue();
+
+        Assertions.assertEquals(expectedTitle, currentInput.title());
+        Assertions.assertEquals(expectedDescription, currentInput.description());
+        Assertions.assertEquals(expectedLaunchYear.getValue(), currentInput.launchedAt());
+        Assertions.assertEquals(expectedDuration, currentInput.duration());
+        Assertions.assertEquals(expectedOpened, currentInput.opened());
+        Assertions.assertEquals(expectedPublished, currentInput.published());
+        Assertions.assertEquals(expectedRating.getName(), currentInput.rating());
+        Assertions.assertEquals(expectedCategories, currentInput.categories());
+        Assertions.assertEquals(expectedGenres, currentInput.genres());
+        Assertions.assertEquals(expectedMembers, currentInput.members());
+        Assertions.assertTrue(currentInput.getVideo().isEmpty());
+        Assertions.assertTrue(currentInput.getTrailer().isEmpty());
+        Assertions.assertTrue(currentInput.getBanner().isEmpty());
+        Assertions.assertTrue(currentInput.getThumbnail().isEmpty());
+        Assertions.assertTrue(currentInput.getThumbnailHalf().isEmpty());
     }
 
 }
