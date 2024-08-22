5792bedbdcb90169ef38e6f4f084b1ecacccacb6
Rafhael <rafhael.dev@gmail.com>
2024-08-22 13:51:34 -0300

    ci: elasticsearch

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 docker network create adm_videos_services
+docker network create elastic
 
 mkdir -m 777 .docker
 mkdir -m 777 .docker/keycloak
+mkdir -m 777 .docker/es01
 
-docker compose -f services/docker-compose.yml up
+docker compose -f services/docker-compose.yml up -d
+docker compose -f elk/docker-compose.yml up -d
 
 echo "Starting containers..."
 sleep 20
